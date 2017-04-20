df390a709259c15a157cb4e1b7be8ecdb92ebfe2
Nik Butenko <nik@butenko.me>
2017-04-20 18:44:03 +1000

    Update README with link to monorepo

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-interval 
+
+All code moved to https://github.com/nkbt/react-works/tree/master/packages/interval
--- packages/interval/src/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const ReactInterval = require('./Component').ReactInterval;
-
-ReactInterval.ReactInterval = ReactInterval;
-
-module.exports = ReactInterval;
